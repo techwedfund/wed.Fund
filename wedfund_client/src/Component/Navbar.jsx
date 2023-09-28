de6ef2af37c304119e0b93c91c0975ee3246d144
Ankit Mishra <144350510+techwedfund@users.noreply.github.com>
2023-09-28 08:29:15 +0530

    initial commit

--- wedfund_client/src/Component/Navbar.jsx
+++ wedfund_client/src/Component/Navbar.jsx
@@ -27,29 +27,29 @@ function Navbar () {
         <>
             <nav className="navbar navbar-expand-lg fixed-top">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/"><img src={logo} className='img-fluid logo' alt='logo' />WedFund</a>
+                    <a className="navbar-brand" href="/"><img src={logo} rel="noopener noreferrer" className='img-fluid logo' alt='logo' />WedFund</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                        <a className={`nav-link ${location.pathname==="/"? "active": ""} px-4`} href="/" >Home</a>
+                        <a className={`nav-link ${location.pathname==="/"? "active": ""} px-4`} rel="noopener noreferrer" href="/" >Home</a>
                         </li>
                         <li className="nav-item">
-                        <a className={`nav-link ${location.pathname==="/about"? "active": ""} px-4` }  href="/about" >About Us</a>
+                        <a className={`nav-link ${location.pathname==="/about"? "active": ""} px-4` } rel="noopener noreferrer"  href="/about" >About Us</a>
                         </li>
                         <li className="nav-item">
-                        <a className={`nav-link ${location.pathname==="/how-it-works"? "active": ""} px-4` } onClick={redirectHowWorks}  href="/#how-It-Works">How it Works</a>
+                        <a className={`nav-link ${location.pathname==="/how-it-works"? "active": ""} px-4` } rel="noopener noreferrer" onClick={redirectHowWorks}  href="/#how-It-Works">How it Works</a>
                         </li>
                         <li className="nav-item">
-                        <a className={`nav-link ${location.pathname==="/careers"? "active": ""} px-4` }  href="/careers" >Careers</a>
+                        <a className={`nav-link ${location.pathname==="/careers"? "active": ""} px-4` } rel="noopener noreferrer"  href="/careers" >Careers</a>
                         </li>
                         <li className="nav-item">
-                        <a className="nav-link px-4" href="https://www.lendingkart.com/dashboard/auth" target='_blank'>Track Your Application</a>
+                        <a className="nav-link px-4" href="https://www.lendingkart.com/dashboard/auth" rel="noopener noreferrer" target='_blank'>Track Your Application</a>
                         </li>
                         <li className="nav-item">
-                        <a className={`nav-link ${location.pathname==="/#contact"? "active": ""} px-4` }  href="/#contact" onClick={redirectFooter}>Contact Us</a>
+                        <a className={`nav-link ${location.pathname==="/#contact"? "active": ""} px-4` }  rel="noopener noreferrer"  href="/#contact" onClick={redirectFooter}>Contact Us</a>
                         </li>
                     </ul>
                     </div>
