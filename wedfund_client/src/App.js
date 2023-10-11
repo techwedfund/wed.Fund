aa942eef75e97d6d0015c57dfc8dd47d60c651cb
techwedfund <144350510+techwedfund@users.noreply.github.com>
2023-10-11 10:58:14 +0530

    initial commit

--- wedfund_client/src/App.js
+++ wedfund_client/src/App.js
@@ -10,14 +10,24 @@ import {
   Routes,
 }from 'react-router-dom';
 import GoToTop from "./Component/GoToTop";
+import LoadingBar from 'react-top-loading-bar'
+import { useState } from "react";
+
 
 function App() {
+
+  const [progress, setProgress] = useState(100)
 
   return (
     <>
 
     <Router>
       <Navbar />
+      <LoadingBar
+        color='#6c0505'
+        progress={progress}
+        onLoaderFinished={() => setProgress(0)}
+      />
       <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
